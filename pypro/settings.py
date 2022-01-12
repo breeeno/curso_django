1f86c332f4f8162e7421a230b8f3be3089fcf1fe
Estudo <bx.sptf@gmail.com>
2022-01-11 21:48:20 -0300

    - Tentativa de correção de .env

--- pypro/settings.py
+++ pypro/settings.py
@@ -21,7 +21,7 @@ from decouple import config
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6)ctw!i=+%$$_ki)si$=j9ivjl+95h+^zg5_=&*)+!+s8!3&3='
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
