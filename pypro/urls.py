27bc8d1dee2fbc58ad1d14f838ae70e2b1cde12e
Estudo <bx.sptf@gmail.com>
2022-01-20 13:08:09 -0300

    - Corrigido path

--- pypro/urls.py
+++ pypro/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home', home, name='home')
+    path('', home, name='home')
 ]
 if settings.DEBUG:
     import debug_toolbar
