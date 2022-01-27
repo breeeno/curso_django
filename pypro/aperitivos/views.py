6b8befc81b88525d94ca59e48cd8ae5f277da74e
Breno <bx.sptf@gmail.com>
2022-01-27 13:10:17 -0300

    - Implementado youtube

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
 class Video:
-    def __init__(self, slug, titulo, vimeo_id):
+    def __init__(self, slug, titulo, youtube_id):
         self.slug = slug
         self.titulo = titulo
-        self.vimeo_id = vimeo_id
+        self.vimeo_id = youtube_id
 videos = [
-    Video( 'motivacao', 'Video Aperitivo: Motivação', 669457570),
-    Video( 'instalacao-windows', 'Instalação Windows', 669916676),
+    Video( 'motivacao', 'Video Aperitivo: Motivação', 'F0cxaoZovy0' ),
+    Video( 'instalacao-windows', 'Instalação Windows', 'IMCMip6kC40'),
 ]
 
 videos_dct = {v.slug: v for v in videos}
