1cc9c50d8580026d13b7b0d7a483c8224e1b3b99
Breno <bx.sptf@gmail.com>
2022-01-28 00:09:29 -0300

    - Corrigido link do youtube - Atualizada dependências - Gerado link para página de indice dos vídeos

--- pypro/aperitivos/tests/test_video.py
+++ pypro/aperitivos/tests/test_video.py
@@ -3,7 +3,6 @@ from django.urls import reverse
 from pypro.django_assertions import assert_contains
 from pypro.aperitivos.views import video
 
-
 @pytest.fixture
 def resp(client):
     return client.get(reverse('aperitivos:video', args=('motivacao',)))
@@ -18,4 +17,4 @@ def test_titulo_video(resp):
 
 
 # def test_conteudo_video(resp):
-#     assert_contains(resp, f'<iframe src="https://www.youtube.com/embed/{video.youtube_id}"')
+#     assert_contains(resp, f'src="https://www.youtube.com/embed/{video.youtube_id}"')
