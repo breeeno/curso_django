72cc23650007067157d78421b0a67f4893929e8d
Breno <bx.sptf@gmail.com>
2022-01-31 12:03:56 -0300

    - Implementado testes com o db - Implementado features de interação com o db - flake8 pra manter as coisas como na pep8 - dependencias atualizadas

--- pypro/aperitivos/tests/test_video.py
+++ pypro/aperitivos/tests/test_video.py
@@ -3,18 +3,35 @@ from django.urls import reverse
 from pypro.django_assertions import assert_contains
 from pypro.aperitivos.views import Video
 
+
 @pytest.fixture
-def resp(client):
-    return client.get(reverse('aperitivos:video', args=('motivacao',)))
+def video(db):
+    v = Video(slug='motivacao', titulo='Video Aperitivo: Motivação', youtube_id='F0cxaoZoy0')
+    v.save()
+    return v
+
+
+@pytest.fixture
+def resp(client, video):
+    return client.get(reverse('aperitivos:video', args=(video.slug,)))
+
+
+@pytest.fixture
+def resp_video_nao_encontrado(client, video):
+    return client.get(reverse('aperitivos:video', args=(video.slug+'video_nao_existente',)))
+
+
+def test_status_code_video_nao_encontado(resp_video_nao_encontrado):
+    assert resp_video_nao_encontrado.status_code == 404
 
 
 def test_status_code(resp):
     assert resp.status_code == 200
 
 
-def test_titulo_video(resp):
-    assert_contains(resp, '>Video Aperitivo: Motivação</h1>')
+def test_titulo_video(resp, video):
+    assert_contains(resp, video.titulo)
 
 
-# def test_conteudo_video(resp):
-#     assert_contains(resp, f'src="https://www.youtube.com/embed/{Video.youtube_id}"')
+def test_conteudo_video(resp, video):
+    assert_contains(resp, f'src="https://www.youtube.com/embed/{video.youtube_id}"')
--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,18 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from pypro.aperitivos.models import Video
 
 videos = [
-    Video(slug ='motivacao', titulo='Video Aperitivo: Motivação', youtube_id='F0cxaoZoy0'),
-    Video(slug ='instalacao-windows', titulo='Instalação Windows', youtube_id='IMCMip6k40'),
+    Video(slug='motivacao', titulo='Video Aperitivo: Motivação', youtube_id='F0cxaoZoy0'),
+    Video(slug='instalacao-windows', titulo='Instalação Windows', youtube_id='IMCMip6k40'),
 ]
 
 videos_dct = {v.slug: v for v in videos}
+
 
 def indice(request):
     return render(request, 'aperitivos/indice.html', context={'videos': videos})
 
 
 def video(request, slug):
-    video = videos_dct[slug]
+    video = get_object_or_404(Video, slug=slug)
     return render(request, 'aperitivos/video.html', context={'video': video})
-
