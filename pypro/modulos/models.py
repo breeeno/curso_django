7f75847c5938203199d7a9fc5ec0dde7f12d22e9
Breno <bx.sptf@gmail.com>
2022-02-08 12:47:55 -0300

    - Implementar geração no banco de dados

--- pypro/modulos/models.py
+++ pypro/modulos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from ordered_model.models import OrderedModel
 
 
@@ -7,9 +8,16 @@ class Modulo(OrderedModel):
     publico = models.TextField(null=True)
     descricao = models.TextField(null=True)
     order = models.PositiveIntegerField(("order"), editable=False, db_index=True, null=True)
+    slug = models.SlugField(unique=True)
+
 
     class Meta(OrderedModel.Meta):
         pass
 
+
     def __str__(self):
         return self.titulo
+
+
+    def get_absolute_url(self):
+        return reverse('modulos:detalhe', kwargs={'slug':self.slug})
