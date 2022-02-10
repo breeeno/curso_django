28c6dbdb565cdfd7fb3b62c5fc47b0940b720a58
Breno <bx.sptf@gmail.com>
2022-02-10 00:34:47 -0300

    - Criado testes referentes ao links das aulas

--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -6,3 +6,6 @@ def detalhe(request, slug):
     modulo = facade.encontrar_modulo(slug)
     aulas = facade.listar_aulas_de_modulos_ordenados(modulo)
     return render(request, 'modulos/modulos_detalhes.html', {'modulo': modulo, 'aulas':aulas})
+
+def aula(request, slug):
+    pass
