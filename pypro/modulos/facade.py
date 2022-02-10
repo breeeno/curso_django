78511d5b949915644f9a137ac58a42da710e01ba
Breno <bx.sptf@gmail.com>
2022-02-10 00:51:54 -0300

    - Criado página de detalhes da aula

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -1,5 +1,5 @@
 from typing import List
-from pypro.modulos.models import Modulo
+from pypro.modulos.models import Modulo, Aula
 
 
 def listar_modulos_ordenados()-> List[Modulo]:
@@ -17,3 +17,7 @@ def encontrar_modulo(slug:str) -> Modulo:
 
 def listar_aulas_de_modulos_ordenados(modulo: Modulo):
     return list(modulo.aula_set.order_by('order').all())
+
+
+def encontrar_aula(slug):
+    return Aula.objects.get(slug=slug)
