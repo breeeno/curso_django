36b9a6ae2ee448b2b8ed6c7ac8b1e9910bf02404
Breno <bx.sptf@gmail.com>
2022-02-15 16:12:40 -0300

    - Implementado redirecionamento correto

--- pypro/base/tests/test_login.py
+++ pypro/base/tests/test_login.py
@@ -1,5 +1,8 @@
+import model_bakery.baker
 import pytest
 from django.urls import reverse
+from model_bakery import baker
+
 
 @pytest.fixture
 def resp(client, db):
@@ -8,3 +11,19 @@ def resp(client, db):
 def test_login_form_page(resp):
     assert resp.status_code == 200
 
+@pytest.fixture
+def usuario(db, django_user_model):
+    usuario_modelo = baker.make(django_user_model)
+    senha = 'senha'
+    usuario_modelo.set_password(senha)
+    usuario_modelo.save()
+    usuario_modelo.senha_plana = senha
+    return usuario_modelo
+
+@pytest.fixture()
+def resp_post(client, usuario):
+    return client.post(reverse('login'),
+    {'username':usuario.email, 'password':usuario.senha_plana})
+
+def test_login_redirect(resp_post):
+    assert resp_post.status_code == 302
