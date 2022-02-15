8c572fd3d4c4a8c7cf531c8e2b6440bee5fbad88
Breno <bx.sptf@gmail.com>
2022-02-15 12:36:47 -0300

    - Finalizado implementação de matrícula

--- pypro/turmas/admin.py
+++ pypro/turmas/admin.py
@@ -2,8 +2,16 @@ from django.contrib import admin
 from pypro.turmas.models import Turma
 # Register your models here.
 
+class MatriculaInLine(admin.StackedInline):
+    model = Turma.alunos.through
+    extra = 1
+    readonly_fields = ('data',)
+    autocomplete_fields = ('usuario',)
+    ordering = ('-data',)
+
 @admin.register(Turma)
 class TurmaAdmin(admin.ModelAdmin):
+    inlines = [MatriculaInLine]
     list_display = ('nome', 'slug', 'inicio', 'fim')
     prepopulated_fields = {'slug':('nome',)}
     ordering = ('-inicio',)
