3301d6e2e89b6189bd51a6f09af50cb6254422f6
Breno <bx.sptf@gmail.com>
2022-02-15 11:53:19 -0300

    - Criado migração referente a matricula dos alunos

--- pypro/turmas/models.py
+++ pypro/turmas/models.py
@@ -8,3 +8,9 @@ class Turma(models.Model):
     slug = models.SlugField(max_length=64)
     inicio = models.DateField()
     fim = models.DateField()
+    alunos = models.ManyToManyField(get_user_model(), through='Matricula')
+
+class Matricula(models.Model):
+    data = models.DateTimeField(auto_now_add=True)
+    usuario = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    turma = models.ForeignKey(Turma, on_delete=models.CASCADE)
