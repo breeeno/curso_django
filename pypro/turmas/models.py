6232789c992139f28fd7e67d401c3884dbcb711c
Breno <bx.sptf@gmail.com>
2022-02-15 12:14:16 -0300

    - Criado restrição de matricula

--- pypro/turmas/models.py
+++ pypro/turmas/models.py
@@ -10,7 +10,12 @@ class Turma(models.Model):
     fim = models.DateField()
     alunos = models.ManyToManyField(get_user_model(), through='Matricula')
 
+
 class Matricula(models.Model):
     data = models.DateTimeField(auto_now_add=True)
     usuario = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     turma = models.ForeignKey(Turma, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = [['usuario', 'turma']]
+        ordering = ['turma', 'data']
