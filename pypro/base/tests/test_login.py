368ce5891a6ff863b8623abdc98698841d26feea
Breno <bx.sptf@gmail.com>
2022-02-15 21:39:55 -0300

    - Implementado botão de sair

--- pypro/base/tests/test_login.py
+++ pypro/base/tests/test_login.py
@@ -22,20 +22,25 @@ def usuario(db, django_user_model):
     usuario_modelo.senha_plana = senha
     return usuario_modelo
 
+
 @pytest.fixture()
 def resp_post(client, usuario):
     return client.post(reverse('login'),
     {'username':usuario.email, 'password':usuario.senha_plana})
 
+
 def test_login_redirect(resp_post):
     assert resp_post.status_code == 302
+
 
 @pytest.fixture
 def resp_home(client, db):
     return client.get(reverse('base:home'))
 
+
 def test_botao_entrar_disp(resp_home):
     assert_contains(resp_home, 'Entrar')
+
 
 def test_link_login(resp_home):
     assert_contains(resp_home, 'Entrar')
@@ -45,8 +50,18 @@ def test_link_login(resp_home):
 def resp_home_com_usuario_logado(client_com_usuario_logado, db):
     return client_com_usuario_logado.get(reverse('base:home'))
 
+
 def test_botao_entrar_indisponivel(resp_home_com_usuario_logado):
     assert_not_contains(resp_home_com_usuario_logado, 'Entrar')
 
+
 def test_link_login_indisponivel(resp_home_com_usuario_logado):
     assert_not_contains(resp_home_com_usuario_logado, 'Entrar')
+
+
+def test_botao_sair_disponivel(resp_home_com_usuario_logado):
+    assert_contains(resp_home_com_usuario_logado, 'Sair')
+
+
+def test_nome_usuario_logado_disponivel(resp_home_com_usuario_logado, usuario_logado):
+    assert_contains(resp_home_com_usuario_logado, usuario_logado.first_name)
--- pypro/conftest.py
+++ pypro/conftest.py
@@ -4,7 +4,7 @@ from model_bakery import baker
 
 @pytest.fixture
 def usuario_logado(db, django_user_model):
-    usuario_logado = baker.make(django_user_model)
+    usuario_logado = baker.make(django_user_model, first_name='Beltrano')
     return usuario_logado
 
 @pytest.fixture
