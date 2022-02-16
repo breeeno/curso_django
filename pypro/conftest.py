84e87189742ff5df75669b33167df71906b12241
Breno <bx.sptf@gmail.com>
2022-02-15 21:22:57 -0300

    - Implementado testes com o usuário logado

---
+++ pypro/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from model_bakery import baker
+
+
+@pytest.fixture
+def usuario_logado(db, django_user_model):
+    usuario_logado = baker.make(django_user_model)
+    return usuario_logado
+
+@pytest.fixture
+def client_com_usuario_logado(usuario_logado, client):
+    client.force_login(usuario_logado)
+    return client
